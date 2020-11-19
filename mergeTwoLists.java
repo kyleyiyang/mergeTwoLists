3b5334ef97e878ec67df9da9da97f82439699858
kyleyiyang <kyleyiyang@gmail.com>
2020-11-18 22:54:09 -0500

    Update mergeTwoLists.java

--- mergeTwoLists.java
+++ mergeTwoLists.java
@@ -11,29 +11,19 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1==null && l2==null) return null;
-        ListNode ans=new ListNode();
+        //ListNode ans=new ListNode();
         Stack<ListNode> q = new Stack<>();
         while (l1!=null || l2!=null) {
             
-                if (l2==null || l1.val<=l2.val)  q.push(l1);l1=l1.next;
-                if (l1==null || l1.val>l2.val) q.push(l2);l2=l2.next;
-            /*if (l1!=null && l2!=null) {
-                if (l1.val<=l2.val) {
+                if (l2==null || (l1!=null && l2!=null && l1.val<=l2.val)) {
                     q.push(l1);
                     l1=l1.next;
-                } else {
+                } else if (l1==null || (l1!=null && l2!=null && l1.val>l2.val)){ 
                     q.push(l2);
                     l2=l2.next;
                 }
-            } else if (l1!=null && l2==null) {
-                q.push(l1);
-                l1=l1.next;
-            } else if (l1==null && l2!=null) {
-                q.push(l2);
-                l2=l2.next;
-            }*/
         }
-        System.out.println(q);
+        ListNode ans=q.pop();
         while (!q.empty()) {
             ListNode temp = q.pop();
             ans=new ListNode(temp.val,ans);
